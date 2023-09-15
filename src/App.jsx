a17efb19d8c7fee890165fab617534d56f9d1bc2
Vaishnavi Raut <vaishnavidraut97@gmail.com>
2023-09-15 12:04:44 +0530

    Bootstrap link is added into index.html and style component is added in About.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Home from './pages/Home'
+import About from './pages/About';
 
 const App = () => {
   return (
    <>
-   <Home />
+   <About />
     </>
   )
   
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const About = () => {
   return (
-    <div>This is About page</div>
+   <Wrapper>
+    <div className='des'>
+      <h1>Hello</h1>
+
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+   </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+
+h1{
+  font : 15px;
+}
+`;
 
 export default About
